e909c9e058b9b92a8f136e55cd1b7a6628c8c6a8
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2020-09-26 11:23:57 +0800

    20200926更新3

--- rt_liu/0905/test.py
+++ rt_liu/0905/test.py
@@ -3,38 +3,40 @@
 # python的作者是Guido
 # python的官网：www.python.org
 # 在python中，井号#是注释，或者使用'''三个单引号，表示多行注释
-# python中，使用缩进表示在方法体中，没有{}。
+# python中，使用缩进表示在方法体中，没有{}
 # 语法与java和javascript的综合体相似
 
 '''
-
     变量和数据类型
-    数字字母和下划线，并且数字不能开头
-    关键字不能用
-    1.对于变量名，见名知意
-    2.不要用英文和中文拼音混写
-    3.多写注释！！！！
-    4.建议采用骆驼标记法userName
+        数字字母和下划线，并且数字不能开头
+        关键字不能用
+            1.对于变量名，见名知意
+            2.不要用英文和中文拼音混写
+            3.多写注释！！！！
+            4.建议采用骆驼标记法userName
 
     数据类型
-    1.数字类型  1.int 2.long 3.float 4.复数complex
-    2.字符串类型
-    3.布尔类型  boolean True和False
-    4.List 列表
-    5.Tuple 元组（是一个只能用来读取的列表，不能添加，删除和修改）
-    6.Dictionary 字典（更接近Java中的map）
+        1.数字类型  1.int 2.long 3.float 4.复数complex
+        2.字符串类型 string
+        3.布尔类型  boolean True和False
+        4.List 列表
+        5.Tuple 元组（是一个只读的列表，不能添加，删除和修改）
+        6.Dictionary 字典（更接近Java中的map）
 
     一般是通过type()来判断变量是什么类型
+        price = float(input("苹果的单价是多少:"))
+        print(type(price))
 
     标识符
-    就是变量名：数字，字母和下划线组成，并且不能用数字开头。
-    ​起名的原则：见名知意
-    ​命名法：骆驼命名法，第一个单词的首字母小写，从第二个单词开始每个单词的首字母都是大写
+        就是变量名：数字，字母和下划线组成，并且不能用数字开头。
+        ​起名的原则：见名知意
+        ​命名法：骆驼命名法，第一个单词的首字母小写，从第二个单词开始每个单词的首字母都是大写
 
     ​关键字：
-    def ,if, try, pass....等都是不可以用的
+        def ,if, try, pass....等都是不可以用的
 
-    输出：print("打印信息")，格式化输出，print("姓名:%s,年龄是%d,身高是%f" % ('张三', 23, 193))
+    输出：print("打印信息")，格式化输出，
+        print("姓名:%s,年龄是%d,身高是%f" % ('张三', 23, 193))
 
     | 符号 | 说明 |
     |:---:|:---------------------------------: |
@@ -47,8 +49,8 @@
     | % x % X | x的大小写决定16进制中的字母的大小写 |
 
     输出时需要缩进或者换行的 \t和 \n符号
-    缩进: print("张三\t李四")    一行打印，但是有一个tab的空格
-    换行: print("张三\n李四")    分两行打印
+        缩进: print("张三\t李四")    一行打印，但是有一个tab的空格
+        换行: print("张三\n李四")    分两行打印
 
     | 函数 | 说明 |
     |:-----: |:---------------------: |
@@ -59,39 +61,45 @@
     | ord() | 将字符转成整数(ASCII码) |
 
     输出：
-    print()
+        print()
     格式化输出：
-    %d 表示整数 常用
-    %s 表示字符串 常用
-    %c 表示字符
-    %o 八进制整数
-    %x 十六进制
-    %X 十六进制
-    %f 浮点数 常用
+        %d 表示整数 常用
+        %s 表示字符串 常用
+        %c 表示字符
+        %o 八进制整数
+        %x 十六进制
+        %X 十六进制
+        %f 浮点数 常用
 
     输入：
-    使用input函数，返回值都是字符串
-    如果需要进行数学计算的话，需要转换为int或者float
-    例如：
-    age = int(input("请输入您的年龄:"))
-    price = float(input("苹果的单价是多少:"))
-    str() 转换为字符串
-    list(x) 将x变成一个列表
-    chr(x) 将一个整数转换为一个字符
-    ord(x) 将一个字符转换为对应的整数
+        使用input函数，返回值都是字符串
+        如果需要进行数学计算的话，需要转换为int或者float
+        例如：
+            age = int(input("请输入您的年龄:"))
+            price = float(input("苹果的单价是多少:"))
+            str()   转换为字符串
+            list(x) 将x变成一个列表
+            chr(x)  将一个整数转换为一个字符
+            ord(x)  将一个字符转换为对应的整数
 
     运算符：
-    // 求除法的整数部分
-    % 求除法的余数部分
-    ** 幂运算
-    没有++自增运算符
-    | and | 并且 |
-    | or  | 或者 |
-    | not | 非 |
+        //  求除法的整数部分
+        %   求除法的余数部分
+        **  幂运算
+        没有++自增运算符
+
+        无&&
+        只有  | and | 与 |
+             | or  | 或 |
+            | not | 非 |
+
+    比较运算符：
+        ==,!=,>,<,<>(检查两个操作的数值是否相同，不等返回真)
 
     判断语句
     ```python
         if 条件:
+            # 使用tab缩进，无{}
             pass
         elif 条件:
             pass
@@ -102,30 +110,44 @@
     循环
     ```python
         name = 'hello world'
+
+        # for 循环更类似于foreach的迭代循环
         for n in name:
             print(n)
-        # for 循环更类似于foreach的迭代循环
 
+        # python的循环主要是while
         while 条件:
             pass
-        break
-        continue
+        break   # 停止当前的循环
+        continue    #停止当次的循环
     ```
 
 '''
 
+# 数据类型转换
+# int(),float(),str()
+# ord()将字符转成整数(ASCII码)
+# chr()将整数转成字符(ASCII码)
+print("*"*50)
 print(ord('A'))
 print(chr(97))
+
+# 没有a++, a--
+print("*"*50)
 a = 1
 a += 1
 print(a)
-print(a)
+
+# 运算符
+print("*"*50)
 print(9//2)
 print(9/2)
 print(9%2)
 print(2**3)
 
-age = int(input("请输入您的年龄:"))
+# 输入输出
+print("*"*50)
+age = int(input("请输入您的年龄:"))    # 类型转换
 price = float(input("苹果的单价是多少:"))
 print(age+10)
 print(type(price))
@@ -134,21 +156,44 @@ print("*"*50)
 age = 20
 name = '王大花'
 print(type(12.5))
-print("我名字是\t%s,\n我的年龄是%d\n"%(name,age))
+print("我名字是%s,我的年龄是%d" % (name, age))    # 正确语法
+print("我名字是", name, ",我的年龄是%d", age)    # 会带空格 不可取
+print("我名字是"+str(name)+",我的年龄是"+str(age))    # 转成字符串 繁琐
+# # print("我名字是"+name+"我的年龄是"+age)   在python中是错误语法
 
-print("hh")
+# 判断语句
+print("*"*50)
 a = 10
-if a>10:
+if a > 10:
     print("hh")
-elif a>2:
+elif a > 2:
     print("hh")
 else:
+    # 占位符号
     pass
 name = 'billl'
 name = "gatmes"
-print(0.1+0.2)
+print(0.1+0.2)  #0.30000000000000004
 
-def show(name,a):
+# 循环
+print("*"*50)
+i = 0
+while i < 10:
+   print("h"*i)
+   i += 1
+print("*"*50)
+name = 'xiaoqing liu 7073'
+print("切片", name[0])  # 切片
+for n in name:
+    print(n)
+
+# 函数
+print("*"*50)
+
+
+def show(name, a):
     print(name)
 
-show(123,"哈哈哈")
+
+show(123, "哈哈哈")
+
--- rt_liu/0912/test.py
+++ rt_liu/0912/test.py
@@ -1,61 +1,114 @@
 # 字符串，列表，元组，字典
 
-### 1.字符串
-
-#### 1.1 切片
+# 字符串
+# 切片
 
 # 切片类似于Java中截取字符串函数
 # 切片的语法[起始:结束:步长]
 # 默认的步长是1
 # 注意：选取的区间属于左闭右开，结束位不包含结束位本身
 
-name = 'abcdef'
+print("*"*50)
+name = 'abcdefghijklmn'
 print(name[0])
 print(name[1])
 print(name[2])
 
-print(name[0:3:2])  # 从第零位开始，到3-1位结束，每间隔2个字母取一个字符
-print(name[0:-1])  # -1表示从倒数的第二位开始取值
-print(name[:3])  # 从第零位开始，到3-1位结束 每间隔1个字母取一个字符
-print(name[3:len(name)])  # 从第四位开始，取到字符nanme的最后一位
-print(name[::])  # 取得全部的字符
+print("*"*50)
+print(name[0:7:2])      # 从0开始，到3-1位结束，每间隔2个字母取一个字符
+print(name[0:-1])        # -1表示从倒数的第二位开始取值
+print(name[:3])         # 从0开始，到3-1位结束 每间隔1个字母取一个字符
+print(name[3:len(name)])  # 从第4位开始，取到字符nanme的最后一位
+print(name[::])          # 取得全部的字符
 
-#### 1.2 常用字符串函数
+# 常用字符串函数
+print("*"*50)
+mystr = "hello is world is a good is day"
 
-# | 函数 | 含义 |
-# |:----: |:----------------------------------------------------------: |
 # | find | 检测一个字符串在另一个字符串中的开始的索引位置，没找到返回 - 1，起始位置和结束位置可选 |
+print(mystr.find('world', 0, len(mystr)))
+
 # | index | 检测一个字符串在另一个字符串中的开始的索引位置，没找到会抛出异常，起始位置和结束位置可选 |
+# print(mystr.index('qq', 0, len(mystr)))
+
 # | count | 检测一下在字符串中出现的另一个字符串的个数，推荐和index函数一块使用 |
+print(mystr.count('is'))
+
 # | replace | 替换，将第个参数替换为第二个参数，替换次数为第三个参数，第三个参数可以不写，默认是全部替换 |
+print(mystr.replace('is', 'sss', 2))
+
 # | split | 切割函数，切割以后会变成列表，第一个是切割的字符，第二参数是最大切割值，第二个参数可以不写，返回一个列表 |
+print(mystr.split("is"))    # 返回一个list
+
 # | capitalize | 字符串的第一个单词首字母大写 |
+print(mystr.capitalize())
+
 # | title | 字符串中所有单词的首字母大写 |
+print(mystr.title())
+
 # | upper | 全部变大写 |
+print(mystr.upper())
+
 # | lower | 全部变小写 |
+print(mystr.lower())
+
 # | startswith | 以特定字符开头，如果是返回True, 否者返回False |
+print(mystr.startswith("hello"))
+
 # | endswith | 以特定字符结尾，如果是返回True, 否者返回False |
+print(mystr.endswith("hello"))
+
+str1 = "hello"
 # | ljust | 左对齐，字符不够用空格补位 |
+print(str1.ljust(20))
+
 # | rjust | 右对齐，字符不够用空格补位 |
+print(str1.rjust(20))
+
 # | center | 剧中对齐，字符不够用空格补位 |
+print(str1.center(20))
+
+str2 = "    hahahaha    "
 # | lstrip | 去掉字符串左边的空格 |
+print(str2.lstrip())
+
 # | rstrip | 去掉字符串右边的空格 |
+print(str2.rstrip())
+
 # | strip | 去掉字符串两边的空格 |
-# | partition | 分区，只分为三个部分，返回一个元组 |
+print(str2.strip())
+
+# | partition | 分区，只分为***三个部分***，返回一个元组 |
+print(mystr.partition("is"))    # ()元组   {}字典   []列表
+
+str3 = "hello\nworld"
 # | splitlines | 按行分割，返回一个包含多行的元素的列表 |
+print(str3.splitlines())
+
+str4 = "abc12"
 # | isalpha | 判断字符都是字母，是返回True否则返回False |
+print(str4.isalpha())
+
 # | isdigit | 判断字符都是数字，是返回True否则返回False |
+print(str4.isdigit())
+
 # | isalnum | 判断字符都是字母和数字，是返回True否则返回False |
+print(str4.isalnum())
+
 # | isspace | 判断字符仅包含空格，是返回True否则返回False |
+print(str4.isspace())
+
+s = "_"
+lists = ["bill", "gates", "lao", "wang"]
 # | join | 使用特殊的连接符号将列表连接称为一个新的字符串 |
+print(s.join(lists))
 
-### 2.列表
-
-#### 2.1 什么是列表
+# 列表
+# 什么是列表
 # ​列表类似于Java中的ArrayList
 
-#### 2.2 定义列表的语法：
-
+# 定义列表的语法：
+print("*"*50)
 namesList = ['张三', '李四', '王五']  # 定一个列表并赋值
 wordsList = []  # 定一个空的列表
 
@@ -64,45 +117,73 @@ print(namesList[1])
 print(namesList[2])
 
 # 使用for循环进行打印
+print("*"*50)
 for name in namesList:
     print(name)
 
 # 使用While循环进行打印
+print("*"*50)
+# len求字符串的长度，也可以求列表中元素的个数
 listLength = len(namesList)
 i = 0
 while i < listLength:
     print(namesList[i])
     i += 1
 
-#### 2.3 List的常用操作函数
+# List的常用操作函数
 
-# | 函数 | 说明 |
-# |:------------------: |:----------------------------------------------------------: |
-# | append(object) | 将元素添加到列表的最后，如果追加的是个列表的话，原列表保持不变 |
-# | extend(object) | 将元素添加到列表的最后，如果追加的是个列表的话，则将原来的列表项变成元素，逐一追加到新列表后面 |
-# | insert(index, object) | 将元素，添加到指定的位置上，index是新列表的索引值 |
-# | in | 表示某一个元素在列表中 |
-# | not in | 表示某个元素不在列表中 |
-# | index | 某个元素在列表中的索引位置，没有找到的话，会引发异常 |
-# | count | 某个元素在列表中出现的次数 |
-# | del | 根据下标进行删除 |
-# | pop | 删除最后一个元素 |
-# | remove | 根据元素的值进行删除 |
+# ()元组 {}字典 []list
 
-# 添加
+# 添加 append,extend,insert
+print("*"*50)
 wordsList = []
-list = [1, 2]
+
+# | append(object) | 将元素添加到列表的最后，如果追加的是个列表的话，原列表保持不变 |
+list1 = [1, 2]
 wordsList.append("apple")
-wordsList.append(list)
-wordsList.extend(list)
+print(wordsList)
+wordsList.append(list1)     # 加入一个list
+print(wordsList)
+print(wordsList[1][1])
+
+# | extend(object) | 将元素添加到列表的最后，如果追加的是个列表的话，则将原来的列表项变成元素，逐一追加到新列表后面 |
+list2 = ['a', 'b']
+wordsList.extend(list2)     # 加入原来列表的内容
+print(wordsList)
+
+# | insert(index, object) | 将元素，添加到指定的位置上，index是新列表的索引值 |
 wordsList.insert(0, 'banana')
 print(wordsList)
 
+# | in | 表示某一个元素在列表中 |
+
+
+# | not in | 表示某个元素不在列表中 |
+
+
+# | index | 某个元素在列表中的索引位置，没有找到的话，会引发异常 |
+
+
+# | count | 某个元素在列表中出现的次数 |
+
+
+# | del | 根据下标进行删除 |
+
+
+# | pop | 删除最后一个元素 |
+
+
+# | remove | 根据元素的值进行删除 |
+
+
+
 # 修改
+print("*"*50)
 wordsList[0] = 'orange'
 
 # 查找
 # in 和 not in
+print("*"*50)
 findName = input("随便输入个水果的名字:")
 if findName in wordsList:
     print("真遗憾，居然找到了~~~！")
@@ -132,7 +213,7 @@ findName = input("随便输入个水果的名字:")
 wordsList.remove(findName)
 print(wordsList)
 
-### 3.元组
+# 元组
 
 # 元组和列表本质上是一样的，元组和列表只有一个却别，就是元组只能查看，增删改对于元组来说，都是不可以操作的。
 
